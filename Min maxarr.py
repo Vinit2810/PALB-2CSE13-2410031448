2900ff50a4fab0df72715e17f451f50df4d982eb
Vinit2810 <vinitk2810@gmail.com>
2026-01-23 10:15:29 +0530

    Added Min maxarr.py and Reverse an array.py

---
+++ Min maxarr.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minMAXArray(self,arr);
+        min = arr[0]
+        max = arr[0]
+        for num in arr:
+            if num < min;
+                min = num
+            if num> max;
+                max = num
+        return [min , max]
+obj = Solution()
+arr = [23,4,6,8,]

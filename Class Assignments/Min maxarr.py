97618e67cd4924de4661811c00e64f8a998965df
Vinit2810 <vinitk2810@gmail.com>
2026-01-24 13:31:07 +0530

    updated solution

--- Class Assignments/Min maxarr.py
+++ Class Assignments/Min maxarr.py
@@ -1,12 +1,11 @@
-class Solution:
-    def minMAXArray(self,arr);
+def find_min_max(arr):
         min = arr[0]
         max = arr[0]
         for num in arr:
-            if num < min;
+            if num < min:
                 min = num
-            if num> max;
+            elif num> max:
                 max = num
         return [min , max]
-obj = Solution()
-arr = [23,4,6,8,]
+arr = [1, 4, 3, 5, 8, 6]
+print(find_min_max(arr))

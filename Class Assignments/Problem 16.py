ff4d6f2dd1313af1e60633576737dd6c3a88def3
Vinit2810 <vinitk2810@gmail.com>
2026-02-14 11:31:15 +0530

    add more

--- Class Assignments/Problem 16.py
+++ Class Assignments/Problem 16.py
@@ -3,25 +3,28 @@ def common_elements(arr1, arr2, arr3):
     i = j = k = 0
 
     n1, n2, n3 = len(arr1), len(arr2), len(arr3)
+
     while i < n1 and j < n2 and k < n3:
         if arr1[i] == arr2[j] == arr3[k]:
-            if  not result and result[-1] <= arr1[i]:
+            if not result or result[-1] != arr1[i]:
                 result.append(arr1[i])
-            i = i + 1
-            j = j + 1
-            k = k + 1
+            
+            i += 1
+            j += 1
+            k += 1
+        elif arr1[i] <= arr2[j] and arr1[i] <= arr3[k]:
+            i += 1
+        elif arr2[j] <= arr1[i] and arr2[j] <= arr3[k]:
+            j += 1
         else:
-            min = min(arr1[i], arr2[j], arr3[k])
-            if arr1[i] < min:
-                i = i + 1
-            if arr2[j] < min:
-                j = j + 1
-            if arr3[k] < min:
-                k = k + 1
+            k += 1
+
     return result if result else [-1]
 arr1 = [1, 5, 10, 20, 40, 80]
 arr2 = [6, 7, 20, 80, 100]
 arr3 = [12, 15, 30, 80, 120]
-print(f"Output: {common_elements}")            
+
+print("Output:", common_elements(arr1, arr2, arr3))
+     
 
 
